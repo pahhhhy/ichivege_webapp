36b1ef3b61e1c0adc477fc2261e3f479997961fc
佐々木寛平 <yamada.g304@gmail.com>
2025-09-03 18:59:04 +0000

    商品の写真を投稿できるようにしたいです

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,11 +2,12 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   projectId: "veggieconnect-gk3oe",
   appId: "1:86070425130:web:02adb2af4b3ea681233553",
-  storageBucket: "veggieconnect-gk3oe.firebasestorage.app",
+  storageBucket: "veggieconnect-gk3oe.appspot.com",
   apiKey: "AIzaSyCUb2xz9Ti7wQFieBUWgV-eg7jJvtSGOqw",
   authDomain: "veggieconnect-gk3oe.firebaseapp.com",
   messagingSenderId: "86070425130",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, db };
+export { app, auth, db, storage };
